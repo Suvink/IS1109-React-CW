9405fb6517d7c426bfbd7cee4ad07f93c00cf01f
Suvin Nimnaka <hello@suvin.me>
2023-02-08 18:14:34 +0530

    Added component files

---
+++ src/screens/AboutUsPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.css";
+
+function AboutUsPage(props) {
+
+    return (
+        <div className="aboutus-section">
+            <h1 className="aboutus-title">About Us Page</h1>
+        </div>
+    );
+}
+
+export default AboutUsPage;  
---
+++ src/screens/HomePage/index.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import "./index.css";
+
+function HomePage(props) {
+
+    //Declare the hooks
+    const [counter, setCounter] = useState(0);
+    const [secondCounter, setSecondCounter] = useState(101);
+
+    //Handling after effects
+    useEffect(() => {
+        setSecondCounter(secondCounter - 1)
+    }, [counter]); // bind counter as a dependancy to the useEffect
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div className="home-section">
+            <h1 className="home-title">Count: {counter}</h1>
+            <h1 className="home-title">Second Count: {secondCounter}</h1>
+            <button className="button is-primary" onClick={increment}>Click Me</button>
+        </div>
+    );
+}
+
+export default HomePage;
